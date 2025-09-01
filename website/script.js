1cebb3472fbd3592e2e6d87e865762a2f81c7b54
Anupriya Dayaratne <anupriya.dayaratne@gmail.com>
2025-09-01 12:18:25 +1200

    First commit

---
+++ website/script.js
@@ -0,0 +1,14 @@
+console.log('Hello World');
+
+//window.alert('This is an Alert !');
+
+//this is a comment
+
+/* This is a comment */
+
+
+document.getElementById("myTitle").textContent = `Hello World`;
+
+let x = 250;
+
+document.getElementById("xValue").textContent = `x value is ${x}`;
